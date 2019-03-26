5fbcafb306eba1c24a1ef7c71d65d06571273985
chenjian <chenjianniu@foxmail.com>
2019-03-26 19:33:01 +0800

    添加输出信息

--- hello.js
+++ hello.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 const app = new Koa()
 
-console.log('启动成功')
+
 app.use(ctx=>{
     ctx.body = 'hello world'
 })
-
-app.listen(3001)
+console.log('启动成功 1')
+app.listen(3001,()=>{
+	console.log('启动成功 2')
+})
 
