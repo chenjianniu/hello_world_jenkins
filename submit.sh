4df045d9fb85d745e01a8ff801c132a81c0d47ad
chenjian <chenjianniu@foxmail.com>
2019-03-27 11:09:26 +0800

    添加输出信息

--- submit.sh
+++ submit.sh
@@ -3,3 +3,4 @@
 cd /var/jenkins_home/workspace/jenkins_hello_world_1
 npm i
 nohup node /var/jenkins_home/workspace/jenkins_hello_world_1/hello.js &
+daemonize -E BUILD_ID=dontKillcenter /var/jenkins_home/workspace/jenkins_hello_world_1/submit.sh
